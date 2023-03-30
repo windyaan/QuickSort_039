6149cc603516884e8020dfa4e22d076ebfb984d1
windyaan <windyanurmaretha@gmail.com>
2023-03-30 14:15:12 +0700

    Membuat procedure input

--- quickshort/quicksort/quicksort/quicksort.cpp
+++ quickshort/quicksort/quicksort/quicksort.cpp
@@ -6,3 +6,26 @@ int arr[20];
 int cmp_count = 0;			//number of comparasion
 int mov_count = 0;			//number of data movements
 int n;
+
+void input() {
+	while (true)
+	{
+		cout << "Masukkan panjang element array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 20" << endl;
+	}
+
+	cout << "\n-------------------" << endl;
+	cout << "\nEnter Array Element" << endl;
+	cout << "\n-------------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
+}
