04308dd35592636bb7fb7de193e6dbb6fcbc3cea
Windya Nurmaretha <126752841+windyaan@users.noreply.github.com>
2023-03-30 17:48:21 +0700

    Delete Quicksort.cpp

--- Quicksort/Quicksort/Quicksort/Quicksort.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-//array of integers to hold values
-int arr[20];
-int cmp_count = 0;			//number of comparasion
-int mov_count = 0;			//number of data movements
-int n;
