62b76b04221a6b591b6add7abbcfa0e415e6fa4b
windyaan <windyanurmaretha@gmail.com>
2023-03-30 14:17:26 +0700

    Membuat procedure quick sort

--- quickshort/quicksort/quicksort/quicksort.cpp
+++ quickshort/quicksort/quicksort/quicksort.cpp
@@ -38,3 +38,60 @@ void swap(int x, int y)
 	arr[x] = arr[y];
 	arr[y] = temp;
 }
+
+void q_short(int low, int high)
+{
+	int pivot, i, j;
+	if (low > high)
+		return;												//langkah 1
+
+	//Partition the list into two parts :
+	//One containing elements less that or equal to pivot
+	//Other containing elements greather than pivot
+
+	pivot = arr[low];										//langkah 2
+
+	i = low + 1;											//langkah 3
+	j = high;												//langkah 4
+
+
+	while (i <= j)											//langkah 10
+	{
+		//search for an element greater than pivot
+		while ((arr[i] <= pivot) && (i <= high))			//langkah 5
+		{
+			i++;											//langkah 6
+			cmp_count;
+		}
+		cmp_count++;
+		//search for an element less than or equal to pivot
+		while ((arr[j] > pivot) && (j >= low))				//langkah 7
+		{
+			j--;											//langkah 8
+			cmp_count++;
+		}
+		cmp_count++;
+
+		//langkah 9
+		if (i < j) //if the greater element is on the left of the element 
+		{
+			//swap the element at index of the last element in the sorted list
+			swap(i, j);
+			mov_count++;
+		}
+	}
+	//j now containt the index of the last element in the sorted list
+	if (low < j)
+	{
+		//move the pivot to its correct position in the list
+		swap(low, j);										//langkah 11
+		mov_count++;
+	}
+	//sort the list on the left of pviot using quick sort
+	q_short(low, j - 1);									//langkah 12
+
+	//sort the list on the right of pivot using quick sort
+	q_short(j + 1, high);									//langkah 13
+
+
+}
